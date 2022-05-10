eb9c340e20f9ef574c6df4f5e67acd6f77dc91b2
rohith <rohith.kumar@mail.waldenu.edu>
2022-05-10 16:39:56 +0530

    new changes

---
+++ remote/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { MicrofrontendModule } from './microfrontend/microfrontend.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MicrofrontendModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
